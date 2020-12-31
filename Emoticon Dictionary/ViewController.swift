72d9035ea49208ffd958209bd75cdc58886f04a3
Eline Koelman <elinekoelman@gmail.com>
2020-12-31 18:00:16 +0100

    table view and second screen added

--- Emoticon Dictionary/ViewController.swift
+++ Emoticon Dictionary/ViewController.swift
@@ -7,11 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
 
+
+    @IBOutlet weak var mainOverviewTable: UITableView!
+    
+    var emoticonArray = ["😄","🛠","🔍", "💎", "🤩", "🫁", "🧞‍♂️","👭", "🙈"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emoticonArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emoticonArray[indexPath.row]
+        return cell
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        mainOverviewTable.dataSource = self
+        mainOverviewTable.delegate = self
     }
 
 
